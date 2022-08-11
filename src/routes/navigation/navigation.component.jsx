e0c0d45b9d7163a2980ee01c54a953ed8b7ef5ba
Zack Daniels <zackdaniels2522@gmail.com>
2022-08-11 15:19:46 -0400

    navbar styling

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,22 @@
 import { Fragment } from 'react';
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+
+import './navigation.styles.scss'
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
-        <h1>I am the Navigation bar</h1>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
