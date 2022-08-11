2481e65d7d38fb612af87101cb1fb68b38c67927
Zack Daniels <zackdaniels2522@gmail.com>
2022-08-11 12:42:19 -0400

    Directory Creation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import './categories.styles.scss'
+import Directory from './components/directory/directory.component'
+
 const App =() =>{
 
   const categories = [
     {
       id: 1,
-      title: 'Hats',
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
       id: 2,
-      title: 'Jackets',
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
       id: 3,
-      title: 'Sneakers',
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
       id: 4,
-      title: 'Womens',
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
       id: 5,
-      title: 'Mens',
-    },
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
+  
 
   return (
-    <div className='categories-container'>
-      {categories.map(({title, id}) => (
-        <div key={id} className='category-container'>
-        <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 }
 
