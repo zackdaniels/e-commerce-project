95909f11a42a3d2e2888d1c6bffbfa5de199a2a7
Zack Daniels <zackdaniels2522@gmail.com>
2022-08-12 11:05:48 -0400

    firebase install and sign in component

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,31 @@
+// creates app instance for you based on some type of config
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider
+} from 'firebase/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdgJrkmLGf-L8NF0EytFnWCjlKv3Jwthk",
+  authDomain: "e-commerce-project-364ff.firebaseapp.com",
+  projectId: "e-commerce-project-364ff",
+  storageBucket: "e-commerce-project-364ff.appspot.com",
+  messagingSenderId: "455348278646",
+  appId: "1:455348278646:web:9d49baf32be8446dd95657",
+  measurementId: "G-JB3MX2M3MQ"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
