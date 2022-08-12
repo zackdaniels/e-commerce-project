097f78fbfe9404a1c34225370f5d1ea42dbae8b9
Zack Daniels <zackdaniels2522@gmail.com>
2022-08-12 16:35:02 -0400

    signup form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,9 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import {
   signInWithGooglePopup, 
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -16,6 +15,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   )
 }
