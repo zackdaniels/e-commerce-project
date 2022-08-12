d91ceec70f4e30bff4f5a2fd3f9852624131a24f
Zack Daniels <zackdaniels2522@gmail.com>
2022-08-12 11:55:39 -0400

    added sign in component functionality

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -46,5 +46,26 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   console.log(userDocRef)
 
   const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot)
   console.log(userSnapshot.exists())
+ 
+  //if user data does not exist
+  //create / set the document with the data from userAuth in my collection
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message)
+    }
+  }
+  // if user data exists
+  //return userDocRef
+  return userDocRef;
 }
